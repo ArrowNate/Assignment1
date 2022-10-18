e204d532263935b9189e8c36e81c465110a1af93
ArrowNate <georgeedmon9@gmail.com>
2022-10-17 22:06:18 -0400

    Setting Up Project For Assignment Version 0.0.0.1

---
+++ Assignment 1/Assignment 1/Assignment 1.cpp
@@ -0,0 +1,11 @@
+// Assignment 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <array>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
+
+int main()
